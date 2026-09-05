4aa55a952a7f7b3fa49f37faea8fa4eb30a116b0
Thomas Petzold <tomlux@gmx.de>
2026-09-05 14:06:05 +0200

    Change material-icons-extended version to 1.7.8
    Downgrade material-icons-extended dependency version from 1.9.3 to 1.7.8.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.9.3")
     implementation("androidx.compose.foundation:foundation:1.9.3")
     implementation("androidx.compose.material3:material3:1.4.0")
-    implementation("androidx.compose.material:material-icons-extended:1.9.3")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     debugImplementation("androidx.compose.ui:ui-tooling:1.9.3")
 
     implementation("androidx.room:room-runtime:2.8.4")
